a2e6a255516fdb8999f8c23463e9bb7a368f7deb
Jianbo He <hjbxyz@gmail.com>
2017-10-19 17:33:56 +0800

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf gconv-$1
+mkdir gconv-$1
+
+if [ $1 == "lnx64" ]; then
+
+    g++ *.cpp -o gconv-$1/gconv -s -O2 -std=c++11 -static
+
+elif [ $1 == "win32" ]; then
+
+    i686-w64-mingw32-g++ *.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
+
+elif [ $1 == "win64" ]; then
+
+    x86_64-w64-mingw32-g++ *.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
+
+fi
