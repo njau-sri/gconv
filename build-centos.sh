2b7a91406f015e38cbfa49704b994df895674c46
Jianbo He <hjbreg@users.noreply.github.com>
2018-10-11 14:54:08 +0800

    Update and rename build.sh to build-centos.sh

--- build-centos.sh
+++ build-centos.sh
@@ -9,7 +9,7 @@ if [ $1 == "lnx64" ]; then
     g++ *.cpp -o gconv-$1/gconv -s -O2 -std=c++11 -static
     qmake-qt4 gui
     make
-    strip gconv-gui
+    strip -s gconv-gui
     mv gconv-gui gconv-$1/
 
 elif [ $1 == "win32" ]; then
@@ -17,7 +17,7 @@ elif [ $1 == "win32" ]; then
     i686-w64-mingw32-g++ *.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
     i686-w64-mingw32-qmake-qt4 gui
     make
-    i686-w64-mingw32-strip release/gconv-gui.exe
+    i686-w64-mingw32-strip -s release/gconv-gui.exe
     mv release/gconv-gui.exe gconv-$1/
 
 elif [ $1 == "win64" ]; then
@@ -25,7 +25,7 @@ elif [ $1 == "win64" ]; then
     x86_64-w64-mingw32-g++ *.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
     x86_64-w64-mingw32-qmake-qt4 gui
     make
-    x86_64-w64-mingw32-strip release/gconv-gui.exe
+    x86_64-w64-mingw32-strip -s release/gconv-gui.exe
     mv release/gconv-gui.exe gconv-$1/
 
 fi
