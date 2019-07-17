3acb1da1416de3e62449c7ad7f72f5201690c740
Jianbo He <hjbxyz@gmail.com>
2019-07-17 20:31:40 +0800

    Update build

--- install.sh
+++ install.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-VER=2019.0.dev
+VER=v1.0.dev
 
-rm -rf gconv-$1
-mkdir gconv-$1
+PKG=gconv-$VER-$1
+
+rm -rf $PKG
+mkdir $PKG
+
 make distclean
 
 if [ $1 == "lnx64" ]; then
 
-    g++ src/*.cpp -o gconv-$1/gconv -s -O2 -std=c++11 -static
-    qmake-qt4 src/gui
+    g++ src/*.cpp -o $PKG/gconv -s -O2 -std=c++11 -static
+    qmake-qt5 src/gui
     make
     strip gconv-gui
-    mv gconv-gui gconv-$1/
+    mv gconv-gui $PKG/
 
 elif [ $1 == "win32" ]; then
 
-    i686-w64-mingw32-g++ src/*.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
+    i686-w64-mingw32-g++ src/*.cpp -o $PKG/gconv.exe -s -O2 -std=c++11 -static
     i686-w64-mingw32-qmake-qt4 src/gui
     make release
     i686-w64-mingw32-strip release/gconv-gui.exe
-    mv release/gconv-gui.exe gconv-$1/
+    mv release/gconv-gui.exe $PKG/
 
 elif [ $1 == "win64" ]; then
 
-    x86_64-w64-mingw32-g++ src/*.cpp -o gconv-$1/gconv.exe -s -O2 -std=c++11 -static
+    x86_64-w64-mingw32-g++ src/*.cpp -o $PKG/gconv.exe -s -O2 -std=c++11 -static
     x86_64-w64-mingw32-qmake-qt4 src/gui
     make release
     x86_64-w64-mingw32-strip release/gconv-gui.exe
-    mv release/gconv-gui.exe gconv-$1/
+    mv release/gconv-gui.exe $PKG/
 
 elif [ $1 == "macos" ]; then
 
@@ -36,13 +39,16 @@ elif [ $1 == "macos" ]; then
     export LDFLAGS="-L/usr/local/opt/qt/lib"
     export CPPFLAGS="-I/usr/local/opt/qt/include"
 
-    g++ src/*.cpp -o gconv -O2 -std=c++11
+    g++ src/*.cpp -o $PKG/gconv -O2 -std=c++11
     qmake src/gui
     make
     macdeployqt gconv-gui.app
-    mv gconv gconv-gui.app/Contents/MacOS/
-    mv gconv-gui.app gconv-$1/
+    mv gconv-gui.app $PKG/
 
 fi
 
-tar zcf gconv-${VER}-${1}.tar.gz gconv-$1
+if [[ $1 == win* ]]; then
+    zip -qr $PKG.zip $PKG
+else
+    tar zcf $PKG.tar.gz $PKG
+fi
